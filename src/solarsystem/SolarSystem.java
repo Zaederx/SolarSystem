8e703ca333811378e71e2d68a999001ade5e64be
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-03-05 17:42:04 +0000

    Finished setup method
    - problem external jars cause  - illegal accessexception

--- src/solarsystem/SolarSystem.java
+++ src/solarsystem/SolarSystem.java
@@ -1,14 +1,17 @@
 package solarsystem;
 
+
 import java.awt.BorderLayout;
 import java.awt.Container;
 
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.swing.JFrame;
 import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 
 import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
 import com.sun.j3d.utils.behaviors.mouse.MouseTranslate;
@@ -31,8 +34,24 @@ public class SolarSystem extends JFrame {
 		Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
 		container.add("Center",canvas);
 		BranchGroup scene = createSceneGraph();
+		SimpleUniverse universe = new SimpleUniverse(canvas);
+		universe.getViewingPlatform().setNominalViewingTransform();
+		universe.addBranchGraph(scene);
 		
+		//viewing platform
+		TransformGroup cameraTG = universe.getViewingPlatform().getViewPlatformTransform();
 		
+		//position of viewing platform
+		Vector3f translate = new Vector3f();
+		Transform3D T3D = new Transform3D();
+	
+		//move along z axis by 10.0f ("move away from the screen")
+		translate.set(0.0f, 0.0f, 10.0f);
+		T3D.setTranslation(translate);
+		cameraTG.setTransform(T3D);
+		setTitle("Step 1: A simple cube");
+		setSize(512,512);
+		setVisible(true);
 	}
 
 	public BranchGroup createSceneGraph () {
