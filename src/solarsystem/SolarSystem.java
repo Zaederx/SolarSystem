72bb2421eeedb0677e3976b82e2b9b5c66e57174
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-03-05 16:40:48 +0000

    Created Scene Graph
    -created the scene graph
    -still nedd to finishe off setup method

--- src/solarsystem/SolarSystem.java
+++ src/solarsystem/SolarSystem.java
@@ -6,9 +6,14 @@ import java.awt.Container;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.TransformGroup;
 import javax.swing.JFrame;
 import javax.vecmath.Point3d;
 
+import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.behaviors.mouse.MouseTranslate;
+import com.sun.j3d.utils.behaviors.mouse.MouseZoom;
+import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 public class SolarSystem extends JFrame {
@@ -38,8 +43,42 @@ public class SolarSystem extends JFrame {
 		BranchGroup objRoot = new BranchGroup();
 		
 		//transform group for the branch group
+		TransformGroup mainTG = new TransformGroup();
+		mainTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		mainTG.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+		
+		//create 3D shape
+		ColorCube colorCube = new ColorCube();
+		
+		//make edge relations with the scene graph nodes
+		objRoot.addChild(mainTG);
+		mainTG.addChild(colorCube);
+		
+		//Create rotation behaviour
+		MouseRotate behaviourRot = new MouseRotate();
+		behaviourRot.setTransformGroup(mainTG);
+		objRoot.addChild(behaviourRot);
+		behaviourRot.setSchedulingBounds(bounds);
 		
 		
+		//MouseRotate behaviour node
+		MouseZoom behaviourZoom = new MouseZoom();
+		behaviourZoom.setTransformGroup(mainTG);
+		objRoot.addChild(behaviourZoom);
+		behaviourZoom.setSchedulingBounds(bounds);
+		
+		//Translate Behaviour
+		MouseTranslate behaviourTrans = new MouseTranslate();
+		behaviourTrans.setTransformGroup(mainTG);
+		objRoot.addChild(behaviourTrans);
+		behaviourTrans.setSchedulingBounds(bounds);
+		
+		objRoot.compile();
+		return objRoot;
+	}
+	
+	public static void main (String [] args) {
+		SolarSystem  solarSystem = new SolarSystem();
 	}
 	
 	
