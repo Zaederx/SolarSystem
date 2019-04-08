1f306bbeb09e8a31498e6036901c7ba8969a21a9
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-04-08 16:26:04 +0100

    Step 3 : Add light - incorporated
    Now the universe scene has background and ambient light added to the scene - no noticeable difference visually though :(

--- src/solarsystem/SolarSystem.java
+++ src/solarsystem/SolarSystem.java
@@ -5,11 +5,13 @@ package solarsystem;
 import java.awt.BorderLayout;
 import java.awt.Container;
 
+import javax.media.j3d.AmbientLight;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
 import javax.media.j3d.ColoringAttributes;
+import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.swing.JFrame;
@@ -30,35 +32,70 @@ import com.sun.j3d.utils.universe.SimpleUniverse;
 
 public class SolarSystem extends JFrame {
 
+	public Canvas3D myCanvas3D;
 	
 	public SolarSystem () {
+//		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		Container container = getContentPane();
+//		BorderLayout bL = new BorderLayout();
+//		container.setLayout(bL);
+//		Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+//		container.add("Center",canvas);
+//		BranchGroup scene = createSceneGraph();
+//		SimpleUniverse universe = new SimpleUniverse(canvas);
+//		universe.getViewingPlatform().setNominalViewingTransform();
+//		universe.addBranchGraph(scene);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container container = getContentPane();
-		BorderLayout bL = new BorderLayout();
-		container.setLayout(bL);
-		Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
-		container.add("Center",canvas);
-		BranchGroup scene = createSceneGraph();
-		SimpleUniverse universe = new SimpleUniverse(canvas);
-		universe.getViewingPlatform().setNominalViewingTransform();
-		universe.addBranchGraph(scene);
+		myCanvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+		getContentPane().add("Center", myCanvas3D);
+		SimpleUniverse simpUniv = new SimpleUniverse(myCanvas3D);
 		
 		//viewing platform
-		TransformGroup cameraTG = universe.getViewingPlatform().getViewPlatformTransform();
+		TransformGroup cameraTG = simpUniv.getViewingPlatform().getViewPlatformTransform();
 		
 		//position of viewing platform
 		Vector3f translate = new Vector3f();
 		Transform3D T3D = new Transform3D();
 	
+		BranchGroup scene = createSceneGraph();
+		simpUniv.addBranchGraph(scene);
 		//move along z axis by 10.0f ("move away from the screen")
 		translate.set(0.0f, 0.0f, 10.0f);
 		T3D.setTranslation(translate);
 		cameraTG.setTransform(T3D);
+		addLight(simpUniv);
 		setTitle("Step 1: A simple cube");
-		setSize(512,512);
+		setSize(700,700);
 		setVisible(true);
 	}
 
+	public void addLight(SimpleUniverse su) {
+		BranchGroup bgLight = new BranchGroup();
+		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0),100.0);
+		
+		//set up light in the scene
+		Color3f ambientLColor = new Color3f(0.6f,0.6f,0.6f);
+		AmbientLight amLight = new AmbientLight(ambientLColor);
+		amLight.setBounds(bounds);
+		bgLight.addChild(amLight);
+		
+		//directionaLight
+		Color3f lightColor = new Color3f(1.0f,1.0f,1.0f);
+		Vector3f direction = new Vector3f(-1.0f,0.0f,-0.5f);
+		DirectionalLight light = new DirectionalLight(lightColor,direction);
+		
+		//light works only within set bounds
+		light.setInfluencingBounds(bounds);
+		bgLight.addChild(light);
+		
+		Vector3f direction2 = new Vector3f(1.0f,-1.0f,0.5f);
+		DirectionalLight light2 = new DirectionalLight(lightColor,direction2);
+		light2.setInfluencingBounds(bounds);
+		bgLight.addChild(light2);
+		
+		su.addBranchGraph(bgLight);
+		
+	}
 	public BranchGroup createSceneGraph () {
 		//creates the bound of the universe
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 10.0);
@@ -145,6 +182,7 @@ public class SolarSystem extends JFrame {
 		objRoot.compile();
 		return objRoot;
 	}
+	
 	
 	public static void main (String [] args) {
 		SolarSystem  solarSystem = new SolarSystem();
