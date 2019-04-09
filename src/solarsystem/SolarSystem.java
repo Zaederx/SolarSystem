5efaf20ef7c00817d99c83881dfdf051c83235a8
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-04-09 14:06:32 +0100

    Pulsating sun
    A weird glitch that lead to this interesting outcome... looks coole though :)

--- src/solarsystem/SolarSystem.java
+++ src/solarsystem/SolarSystem.java
@@ -36,6 +36,9 @@ public class SolarSystem extends JFrame {
 
 	public Canvas3D myCanvas3D;
 	
+	/**
+	 * Set's up a solar system on the canvas.
+	 */
 	public SolarSystem () {
 //		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		Container container = getContentPane();
@@ -71,6 +74,10 @@ public class SolarSystem extends JFrame {
 		setVisible(true);
 	}
 
+	/**
+	 * Adds light to the scene.
+	 * @param su - universe you want to add light to
+	 */
 	public void addLight(SimpleUniverse su) {
 		BranchGroup bgLight = new BranchGroup();
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0),100.0);
@@ -98,6 +105,10 @@ public class SolarSystem extends JFrame {
 		su.addBranchGraph(bgLight);
 	}
 	
+	/**
+	 * Creates a new SceneGraph.
+	 * @return
+	 */
 	public BranchGroup createSceneGraph () {
 		//creates the bound of the universe
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 10.0);
@@ -128,47 +139,65 @@ public class SolarSystem extends JFrame {
 		
 		
 		//matrix for translation t1
-		
 		Matrix4d matrix = new Matrix4d();
 		Transform3D mercuryT = new Transform3D();
 		TransformGroup mercuryTG = createTG(0.0,0.0,-5, 2.0,2.0,2.0,mercuryT, matrix);
-//		mercuryTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-//		mercuryTG.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 		Sphere mercury = celestialBody(0.5f, 1f, 0f, 0f);
 		
-		TransformGroup rotTG0 = new TransformGroup();
-		rotTG0.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-		Alpha rotationAlpha = new Alpha(1, 18000);
-		Transform3D yAxis = new Transform3D();
-		RotationInterpolator rotator = new RotationInterpolator(rotationAlpha, rotTG0, yAxis, 0.0f, (float) Math.PI * (1.0f/4));
-		rotator.setSchedulingBounds(bounds);
+		//**Venus***
+		Transform3D venusT = new Transform3D();
+		TransformGroup venusTG = createTG(0.0,0.0,-5, 2.0,2.0,2.0, venusT);
+		Sphere venus = celestialBody(0.5f, 1f, .7f, .5f);
+		//**Venus *** END
 		
+		//*****Rotation*****
+		TransformGroup rotTG1 = new TransformGroup();
+		rotTG1.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		Alpha rotAlpha1 = new Alpha(-1, 18000);
+		Transform3D yAxis = new Transform3D();
+		
+		//float variable controls how much of a rotation
+		RotationInterpolator rotator = new RotationInterpolator(rotAlpha1, rotTG1, yAxis, 0.0f, (float) Math.PI * (3.0f)/4);
+		rotator.setSchedulingBounds(bounds);	
 		Transform3D t = new Transform3D();
 		t.setScale(new Vector3d(2.0,2.0,2.0));
 		t.setTranslation(new Vector3d(0.0,0.0,-5));
 		Transform3D helperT3D = new Transform3D();
 		helperT3D.rotZ(Math.PI);
 		t.mul(helperT3D);
-		t.rotX(Math.PI/2);
+		helperT3D.rotX(Math.PI/2);
 		t.mul(helperT3D);
+		//*****Rotation*** END
 		
-		Transform3D venusT = new Transform3D();
-		TransformGroup venusTG = createTG(0.0,0.0,-5, 2.0,2.0,2.0, venusT);
-		Sphere venus = celestialBody(0.5f, 1f, .7f, .5f);
+		//*****Rotation2*********
+		TransformGroup rotTG2 = new TransformGroup();
+		rotTG2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		Alpha rotAlpha2 = new Alpha(-1, 4000);
+		Transform3D yAxis2 = new Transform3D();
+		
+		RotationInterpolator rotator2 = new RotationInterpolator(rotAlpha2, rotTG2, yAxis2,0.0f, (float) Math.PI * 2.0f);
+		rotator2.setSchedulingBounds(bounds);
+		//****Rotation2******
+		
+		
 		//make edge relations with the scene graph nodes
 		//cube 1 translated -5 along z axis
 		objRoot.addChild(sunTG);
 		sunTG.addChild(sun);//mainTG
-		sunTG.addChild(rotTG0);
-		rotTG0.addChild(mercuryTG);//cubeTG0
-		rotTG0.addChild(rotator);
+		sunTG.addChild(rotTG1);
+//		rotTG1.addChild(mercuryTG);//cubeTG0
+		rotTG1.addChild(rotator);
+		rotTG1.addChild(rotTG2);
+		rotTG2.addChild(rotator2);
+		rotTG2.addChild(mercury);
+//		rotTG2.addChild(mercuryTG);
 //		sunTG.addChild(mercuryTG);
 //		sunTG.addChild(rotator);
-		mercuryTG.addChild(mercury);//cubeTG1
-		mercuryTG.addChild(planetaryRing);
-		mercuryTG.addChild(venusTG);
-//		mercuryTG.addChild(rotator);
-		venusTG.addChild(venus);//cubeTG2
+////		mercuryTG.addChild(mercury);//cubeTG1
+//		mercuryTG.addChild(planetaryRing);
+//		mercuryTG.addChild(venusTG);
+////		mercuryTG.addChild(rotator);
+//		venusTG.addChild(venus);//cubeTG2
 		
 		
 		//Create rotation behaviour
@@ -193,9 +222,44 @@ public class SolarSystem extends JFrame {
 		return objRoot;
 	}
 	
+//	/**
+//	 * 
+//	 * @param rotator
+//	 * @param rotation
+//	 * @param rotationAlpha
+//	 * @param axis
+//	 * @param bounds
+//	 * @return
+//	 */
+//	public TransformGroup rotationTG(RotationInterpolator rotator, float rotation, Alpha rotationAlpha, Transform3D axis,  BoundingSphere bounds ) {
+//		TransformGroup rotTG = new TransformGroup();
+//		rotTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+//		rotator.setSchedulingBounds(bounds);
+//		Transform3D t = new Transform3D();
+//		t.setScale(new Vector3d(2.0,2.0,2.0));
+//		t.setTranslation(new Vector3d(0.0,0.0,-5));
+//		Transform3D helperT3D = new Transform3D();
+//		helperT3D.rotZ(Math.PI);
+//		t.mul(helperT3D);
+//		t.rotX(Math.PI/2);
+//		
+//		return rotTG;
+//	}
+	/**
+	 * Helper method to create Transform groups.
+	 * Helps to make groups that 
+	 * @param transX
+	 * @param transY
+	 * @param transZ
+	 * @param scaleX
+	 * @param scaleY
+	 * @param scaleZ
+	 * @param planet
+	 * @return
+	 */
 	public TransformGroup createTG(double transX, double transY, double transZ,
-									double scaleX, double scaleY,double scaleZ, Transform3D planet) {
-		Transform3D planetT = new Transform3D();
+									double scaleX, double scaleY,double scaleZ, Transform3D planetT) {
+		planetT = new Transform3D();
 		planetT.setTranslation(new Vector3d(transX,transY, transZ));
 		planetT.setScale(new Vector3d(scaleX,scaleY,scaleZ));
 		TransformGroup planetTG = new TransformGroup(planetT);
@@ -203,9 +267,23 @@ public class SolarSystem extends JFrame {
 		return planetTG;
 	}
 	
+	/**
+	 * Helper method to create Transform groups and get it's matrix.
+	 * Good for if you wanted to print the transforms's matrix.
+	 * @param transX
+	 * @param transY
+	 * @param transZ
+	 * @param scaleX
+	 * @param scaleY
+	 * @param scaleZ
+	 * @param planet
+	 * @param matrix
+	 * 
+	 * @return
+	 */
 	public TransformGroup createTG(double transX, double transY, double transZ,
-			double scaleX, double scaleY,double scaleZ,Transform3D planet, Matrix4d matrix) {
-			Transform3D planetT = new Transform3D();
+			double scaleX, double scaleY,double scaleZ,Transform3D planetT, Matrix4d matrix) {
+			planetT = new Transform3D();
 			planetT.setTranslation(new Vector3d(transX,transY, transZ));
 			planetT.setScale(new Vector3d(scaleX,scaleY,scaleZ));
 			planetT.get(matrix);
@@ -213,6 +291,14 @@ public class SolarSystem extends JFrame {
 
 			return planetTG;
 }
+	/**
+	 * Creates a new celestialBody (planet, moon or star).
+	 * @param sphereSize
+	 * @param red
+	 * @param green
+	 * @param blue
+	 * @return
+	 */
 	public Sphere celestialBody(float sphereSize, float red, float green, float blue) {
 		Sphere celestialBody = new Sphere(sphereSize);
 		Appearance appearance = new Appearance();
@@ -225,6 +311,10 @@ public class SolarSystem extends JFrame {
 		return celestialBody;
 	}
 	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main (String [] args) {
 		SolarSystem  solarSystem = new SolarSystem();
 	}
