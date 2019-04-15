0bce712fe379aec9fb5fd502e290d089ddf54645
Zachary Ishmael <zacharyk.ishmael@gmail.com>
2019-04-15 14:24:16 +0100

    Also commented out old code
    Will leave it commented for reference to early coding attempts.

--- src/solarsystem/SolarSystem.java
+++ src/solarsystem/SolarSystem.java
@@ -195,33 +195,33 @@ public class SolarSystem extends JFrame {
 //		Sphere mercury = celestialBody(0.5f, 1f, 0f, 0f);
 
 //		Texture texImage = new TextureLoader("src/solarsystem/mercury.jpg", this).getTexture();
-		File pic = new File("src/solarsystem/mercury.jpg");
-	
-		TextureLoader loader = new TextureLoader("src/textures/mercury.jpg", this);
-//		ImageComponent2D image = loader.getImage();
-		ImageComponent2D image = loader.getScaledImage(256, 256);
-		Texture2D texture = new Texture2D(Texture2D.BASE_LEVEL, Texture2D.RGB, image.getWidth(),image.getHeight());
-
-//		Texture3D texture = new Texture3D(Texture3D.BASE_LEVEL, Texture3D.RGB, image.getWidth(),image.getHeight(), 1 );
-		texture.setImage(0, image);
-		
-		Appearance mTextureApp = new Appearance();
-		mTextureApp.setTexture(texture);
-		
-		TextureAttributes textureAttr = new TextureAttributes();
-		textureAttr.setTextureMode(TextureAttributes.REPLACE);
-		mTextureApp.setTextureAttributes(textureAttr);
-		
-		Material material = new Material();
-		material.setShininess(0f);
-		
-		mTextureApp.setMaterial(material);
-		
-		TexCoordGeneration tcg = new TexCoordGeneration(TexCoordGeneration.OBJECT_LINEAR,
-				TexCoordGeneration.TEXTURE_COORDINATE_2);
-		
-		mTextureApp.setTexCoordGeneration(tcg);
-		mercury.setAppearance(mTextureApp);
+//		File pic = new File("src/solarsystem/mercury.jpg");
+//	
+//		TextureLoader loader = new TextureLoader("src/textures/mercury.jpg", this);
+////		ImageComponent2D image = loader.getImage();
+//		ImageComponent2D image = loader.getScaledImage(256, 256);
+//		Texture2D texture = new Texture2D(Texture2D.BASE_LEVEL, Texture2D.RGB, image.getWidth(),image.getHeight());
+//
+////		Texture3D texture = new Texture3D(Texture3D.BASE_LEVEL, Texture3D.RGB, image.getWidth(),image.getHeight(), 1 );
+//		texture.setImage(0, image);
+//		
+//		Appearance mTextureApp = new Appearance();
+//		mTextureApp.setTexture(texture);
+//		
+//		TextureAttributes textureAttr = new TextureAttributes();
+//		textureAttr.setTextureMode(TextureAttributes.REPLACE);
+//		mTextureApp.setTextureAttributes(textureAttr);
+//		
+//		Material material = new Material();
+//		material.setShininess(0f);
+//		
+//		mTextureApp.setMaterial(material);
+//		
+//		TexCoordGeneration tcg = new TexCoordGeneration(TexCoordGeneration.OBJECT_LINEAR,
+//				TexCoordGeneration.TEXTURE_COORDINATE_2);
+//		
+//		mTextureApp.setTexCoordGeneration(tcg);
+//		mercury.setAppearance(mTextureApp);
 	
 		//***Mercury*** END
 		
